c413512090c4b88850e16cb0ffa562d305de6893
Hector <DAW@DESKTOP-B8E4IF5>
2022-04-04 13:10:23 +0200

    Creado el DAO de Pedido

---
+++ GestionVentasV3Git/src/main/java/com/loren/gestionventasv3/DAO/PedidoDAO.java
@@ -0,0 +1,13 @@
+package com.loren.gestionventasv3.DAO;
+
+import java.sql.Connection;
+
+/**
+ * @author Héctor Utrillas Escapa
+ */
+public class PedidoDAO {
+    //La conexion
+    private Connection conn = ConexionBD.getConnection();
+    
+    
+}
